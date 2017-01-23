fa6e2be177d91a30179062e9822e5e52c6c772d8
Nicolas <nicolascogu@hotmail.com>
2017-01-23 11:56:23 +0100

    comit first Activity

---
+++ AndroidStudioProjects/actividad1/app/src/main/java/com/example/dell/actividad1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dell.actividad1;
+
+/**
+ * Created by DELL on 1/23/2017.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_1);
+    }
+}
