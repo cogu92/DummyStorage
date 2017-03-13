bb98851e127063aea6a3740da67ebce3606a261d
Nicolas <nicolascogu@hotmail.com>
2017-03-13 09:44:42 +0100

    First Comit

---
+++ Desktop/android test/android-maps-utils-master/DummyAdapter/app/src/main/java/com/example/dell/dummyadapter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dell.dummyadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listview= (ListView) findViewById(R.id.list_view_main);
+    }
+}
